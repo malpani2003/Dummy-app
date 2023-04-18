c17f3df19e059229384068e711e5fc8c23ffc8c6
Pranav <pranavmaheshwari123@gmail.com>
2023-04-18 05:30:12 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,120 @@
+# Web Server is used to accept incoming request 
+# 1. GET - going to page
+# 2. POST - Receive data and use it and data come with it
+# 3. DELETE - remove data
+# 4. PUT - update and add data 
+# 5. HEAD AND MUCH MORE
+
+from flask import Flask,jsonify,request
+
+# Created an Object of Flask with a unique name
+app=Flask(__name__)  
+
+stores=[
+    {
+        "name":"Amazon",
+        "items":[
+            {
+                "item":"Earphone",
+                "price":"560"
+            },
+            {
+                "item":"Laptop",
+                "price":"56000"
+            }
+        ]
+    },
+    {
+        "name":"Flipkart",
+        "items":[
+            {
+                "item":"Phone",
+                "price":"15600"
+            },
+            {
+                "item":"Charger",
+                "price":"400"
+            }
+        ]
+    }
+]
+
+# By default route requets are GET in nature
+@app.route("/")
+def home():
+    return jsonify("Hello")
+
+# / is the index route or home route
+@app.route("/store",methods=["POST"])
+def create_store():
+    request_data=request.get_json()
+    new_store={
+        "name":request_data['name'],
+        "items":[]
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+    pass
+
+@app.route("/store/<string:name>")  # /store/some_name then some_name will be assigned to name variable in function
+def get_store(name):
+    Found=0
+    for ele in stores:
+        if ele["name"].lower()==name.lower():
+            Found=1
+            return jsonify(ele)
+        else:
+            continue
+    if(Found==0):
+        return jsonify({
+            "Error":"No Store Found"
+        })
+
+
+@app.route("/store")
+def get_all_store():
+    return jsonify({"stores":stores})  # jsonify convert list to json data
+    # pass
+
+
+@app.route("/store/<string:name>/item")
+def get_item_in_store(name):
+    Found=0
+    for ele in stores:
+        if ele["name"].lower()==name.lower():
+            Found=1
+            return jsonify({"Items":ele["items"]})
+        else:
+            continue
+    if(Found==0):
+        return jsonify({
+            "Error":"No Store Found"
+        })
+
+
+@app.route("/store/<string:name>/item",methods=["POST"])
+def create_item_store(name):
+    request_data=request.get_json() # to get json data from api request
+    for ele in stores:
+        if ele["name"].lower()==name.lower():
+            new_item={
+        "item":request_data["name"],
+        "price":request_data["price"]
+    }
+            ele["items"].append(new_item)
+            return jsonify({"Items":ele["items"]})
+    return jsonify({
+            "Error":"No Store Found"
+        })
+
+if __name__=="__main__":
+    app.run()
+
+
+# {  "name": "Test",
+#      "username": "TheTestUser123",
+#      "email": "TheTestUser123@gmail.com",
+#      "oldPassword":"testcbv323",
+#      "password":"testcbv"
+# }
